73e412fbd300f802cab65ef7195d0749cc378737
Darién Julián Ramírez <ianshalaga@gmail.com>
2018-06-20 23:11:16 -0300

    cambios menores

---
+++ hms2ms.cpp
@@ -0,0 +1,16 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "iostream"
+#include "string"
+#include "pdi_functions.h"
+#include "djr_functions.h"
+
+using namespace cv;
+using namespace std;
+using namespace pdi;
+
+double hms2ms(string tiempo_hms) {
+	
+	
+	
+}
